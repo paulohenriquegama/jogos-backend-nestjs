bd3258ca46346b3b2213eceff4d602b96f5e6978
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-10-20 22:58:24 -0300

    mysql to postgresql

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
   // Interceptors
   app.useGlobalInterceptors(new UnauthorizedInterceptor());
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
